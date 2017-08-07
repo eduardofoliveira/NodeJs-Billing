8e5845dd0bcc2ec32833b296131f2d2d7d81e953
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2017-08-07 17:55:26 -0300

    Processamento recursivo OK

--- processador.js
+++ processador.js
@@ -4,7 +4,11 @@ function searchRate(prefix, callback){
     connection.query('select custo_conexao, segundos_incluidos, custo_minuto, incremento_inicial, increment, prefixo from clientes, tarifas, areas, prefixos where clientes.id = tarifas.fk_id_cliente and areas.id = tarifas.fk_id_area and areas.id = prefixos.fk_id_area and clientes.id = 1 and prefixos.prefixo = ?', [prefix], function(error, result){
         if(error) console.log(`Erro: ${error}`);
         if(result.length == 0){
-            searchRate(prefix.slice(0, prefix.length-1))
+            if(prefix.length == 0){
+                connection.end();
+                return callback("Prefixo não encontrado", null);
+            }
+            searchRate(prefix.slice(0, prefix.length-1), callback);
         }else{
             connection.end();
             return callback(null, result[0]);
