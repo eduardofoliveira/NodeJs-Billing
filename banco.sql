c4a6faee27b41b835ed5dc85abb823707460ffe3
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2017-08-16 18:23:52 -0300

    Insert Origination Rates

--- banco.sql
+++ banco.sql
@@ -90,3 +90,10 @@ and callednum like '0800%' and accountid = ?;
 update cdrs set debit = ?, rate_cost = ? where id = ?;
 
 update accounts set balance = balance - debit where id = ?;
+
+/* Query to insert Origination Rate*/
+INSERT INTO routes (
+    pattern, comment, connectcost, includedseconds, cost, pricelist_id, inc, init_inc, creation_date)
+    VALUES (
+    '^55119.*', 'São Paulo Móvel', 0, 4, 0.69, 5, 6, 30, now()
+);
--- ler-csv.js
+++ ler-csv.js
@@ -2,36 +2,55 @@ var fs = require('fs');
 var csv = require('fast-csv');
 
 var stream = fs.createReadStream("RotasBilling.csv");
-var registros = [];
-var descricao = [];
+var lista = [];
+
+lista.push(['Code', 'Destination', 'Connect Cost', 'Included Seconds', 'Per Minute Cost', 'Initial Increment', 'Increment']);
 
 csv
  .fromStream(stream, {delimiter :';'})
  .on("data", function(data){
- 		//registros.push([data[0], titleCase(data[1]), data[2], data[3]]);
-        if(data[0].indexOf('551199968') == 0){
-            console.log([data[0], titleCase(data[1]), data[2], data[3]]);
-        }
 
-        if(descricao.indexOf(data[2]) > -1){
-
-        }else{
-            if(data[0].indexOf('55') == 0){
-                descricao.push(data[2]);
+        if(data[0].indexOf('55') == 0){
+            if(data[2] == 'Demais Cidades do Brasil - Fixo'){
+                lista.push([data[0], data[2], 0, 4, 0.10, 30, 6]);
+            }
+            if(data[2] == 'Principais Cidades do Interior do Estado de SP - Fixo'){
+                lista.push([data[0], data[2], 0, 4, 0.10, 30, 6]);
+            }
+            if(data[2] == 'Demais Capitais Estaduais - Fixo'){
+                lista.push([data[0], data[2], 0, 4, 0.10, 30, 6]);
+            }
+            if(data[2] == 'Cidades Importantes do Brasil - Fixo'){
+                lista.push([data[0], data[2], 0, 4, 0.10, 30, 6]);
+            }
+            if(data[2] == 'Área Metropolitana da Grande São Paulo - Fixo'){
+                lista.push([data[0], data[2], 0, 4, 0.06, 30, 6]);
+            }
+            if(data[2] == 'Rio de Janeiro, Belo Horizonte, Curitiba e Brasília - Fixo'){
+                lista.push([data[0], data[2], 0, 4, 0.06, 30, 6]);
+            }
+            if(data[2] == 'Gratuíto Nacional'){
+                lista.push([data[0], data[2], 0, 4, 0.00, 30, 6]);
+            }
+            if(data[2] == 'Principais Destinos do Brasil - Móvel'){
+                lista.push([data[0], data[2], 0, 4, 0.49, 30, 6]);
+            }
+            if(data[2] == 'Demais Destinos do Brasil - Móvel'){
+                lista.push([data[0], data[2], 0, 4, 0.49, 30, 6]);
             }
         }
+
  })
  .on("end", function(){
-     descricao.forEach(function(item){
-         console.log(item);
-     });
- });
 
-//function titleCase(str) {
-//     return str.toLowerCase().split(' ').map(function(word) {
-//         return word.replace(word[0], word[0].toUpperCase());
-//     }).join(' ');
-// }
+    var file = fs.createWriteStream('Plano_Basix_Nacional.csv');
+	file.on('error', function(err) { /* error handling */ });
+	lista.forEach(function(v) {
+		file.write(v.join(',') + '\r\n');
+	});
+	file.end();
+
+ });
 
  function titleCase(str) {
    return str.toLowerCase().split(' ').map(function(word) {
